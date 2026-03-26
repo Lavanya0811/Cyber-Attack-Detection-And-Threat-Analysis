ff82d5282f102fd836e00d7031118e2e87f2ddc3
Lavanya0811 <lavanyagurunath470@gmail.com>
2026-03-26 15:58:15 +0530

    deploy fix

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@ from app.main import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
